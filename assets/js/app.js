b07a2772dc9a5e71450207e1f9253b8bd51acb7a
RAIZOxd <kavishkatv99@gmail.com>
2024-08-13 17:44:52 +0530

    test commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+function bitRegister(){
+    console.log("btn clicked");
+}
+
+let btnRegister = document.getElementById("btn-registetr")
+                          .addEventListener("click",bitRegister);
+
